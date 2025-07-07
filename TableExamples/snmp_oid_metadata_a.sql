f757d84c29d6f48c7cee5cf3eea40c54481dc62a
George Leonard <georgelza@gmail.com>
2025-07-07 09:24:05 +0200

    Updated TableExamples table syntax

--- TableExamples/snmp_oid_metadata_a.sql
+++ TableExamples/snmp_oid_metadata_a.sql
@@ -1,11 +1,11 @@
 --
 CREATE TABLE hive.snmp.snmp_oid_metadata (
-    oid_string          VARCHAR(255) PRIMARY KEY,           -- The numerical OID (e.g., ".1.3.6.1.2.1.1.1.0")
-    oid_name            VARCHAR(255),                       -- The human-readable name (e.g., "sysDescr")
-    description         VARCHAR(2000),                      -- The textual description from the MIB
-    syntax_type         VARCHAR(255),                       -- The data type (e.g., "DisplayString", "Integer32")
-    unit                VARCHAR(50),                        -- Optional: units (e.g., "seconds", "bytes")
-    oid_type            VARCHAR(50),                        -- Optional: "scalar", "table", "notification", etc.
+    oid_string          VARCHAR(255) PRIMARY KEY NOT ENFORCED,  -- The numerical OID (e.g., ".1.3.6.1.2.1.1.1.0")
+    oid_name            VARCHAR(255),                           -- The human-readable name (e.g., "sysDescr")
+    description         VARCHAR(2000),                          -- The textual description from the MIB
+    syntax_type         VARCHAR(255),                           -- The data type (e.g., "DisplayString", "Integer32")
+    unit                VARCHAR(50),                            -- Optional: units (e.g., "seconds", "bytes")
+    oid_type            VARCHAR(50),                            -- Optional: "scalar", "table", "notification", etc.
 ) WITH (
     'connector'             = 'redis',
     'hostname'              = 'redis',
